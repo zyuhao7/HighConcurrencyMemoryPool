0c0a9fbb12a5369f630ce0d77836b8dc4a3d157f
myh <zyuhao7@yeah.net>
2025-08-14 09:18:36 +0800

    CentralCache.h

--- CentralCache/CentralCache.h
+++ CentralCache/CentralCache.h
@@ -12,7 +12,7 @@ public:
     }
 
     // 获取一个非空的 span
-    Span *GetOneSpan(SpanList *list, size_t bytes_size);
+    Span *GetOneSpan(SpanList &list, size_t bytes_size);
     // 从中心缓存获取一定数量的对象给 thread cache
     size_t FetchRangeObj(void *&start, void *&end, size_t batchNum, size_t size);
 
